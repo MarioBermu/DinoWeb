e0b42c4a8d15913dea16781725a94d3636a709eb
MarioBermu <147144316+MarioBermu@users.noreply.github.com>
2023-12-26 17:20:25 +0100

    01

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { CalendarioComponent } from '../calendario/calendario.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, CalendarioComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
